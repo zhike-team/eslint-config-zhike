f9c60e46a0a0edcaec2ca644b029811a35312830
Yudu Ban <banyudu@innobuddy.com>
2018-03-30 11:34:59 +0800

    Add jsdoc related rules

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ module.exports = {
       ignoreArrayIndexes: true,
       enforceConst: true,
       ignore: [0, 1, -1]
-    }]
+    }],
+    'require-jsdoc': ['warn', {
+      require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false
+      }
+    }],
+    'valid-jsdoc': 'warn'
   },
 }
