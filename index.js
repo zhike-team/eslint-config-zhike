1a72679d676feb234740d8a310dcfbf4f26b00a4
Yudu Ban <banyudu@innobuddy.com>
2018-05-08 09:28:59 +0800

    allow some magic numbers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     'no-magic-numbers': ['warn', {
       ignoreArrayIndexes: true,
       enforceConst: true,
-      ignore: [0, 1, 2, -1, 7, 10, 24, 60, 100, 1000, 9999]
+      ignore: [0, 1, 2, -1, 7, 10, 12, 24, 60, 100, 360, 365, 1000, 1024, 9999]
     }],
     'require-jsdoc': 'warn',
     'valid-jsdoc': 'warn',
