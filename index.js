442b4603a26411afda52d8ae048f9121690b121e
Yudu Ban <banyudu@innobuddy.com>
2018-09-26 19:29:26 +0800

    remove jsdoc related rules

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ module.exports = {
       enforceConst: true,
       ignore: [0, 1, 2, -1, 7, 10, 12, 24, 60, 100, 360, 365, 1000, 1024, 9999]
     }],
-    'require-jsdoc': 'warn',
-    'valid-jsdoc': 'warn',
     'require-yield': 'error',
     'quote-props': ['error', 'as-needed']
   },
