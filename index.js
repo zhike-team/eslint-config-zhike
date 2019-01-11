477844cc139864c732622d8f58f568a1306d0c05
Yudu Ban <banyudu@innobuddy.com>
2019-01-11 18:54:37 +0800

    add rules max-<xxx> supported in eslint v5

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ module.exports = {
       ignore: [0, 1, 2, -1, 7, 10, 12, 24, 60, 100, 360, 365, 1000, 1024, 9999]
     }],
     'require-yield': 'error',
+    'max-statements': ['error', 75],
+    'max-lines-per-function': ['error', 150],
+    'max-lines': ['error', 2500],
+    'max-depth': ['error', 8],
     'quote-props': ['error', 'as-needed']
   },
 }
